feefe321957f9f6211ac9be77ce0c7172be5c6e6
Dan Sebastian Thrane <dthrane@gmail.com>
2015-12-11 14:17:55 +0100

    Fixes some stuff

---
+++ projekt/our/BioInformatik/src/main/java/dk/sdu/dm847/exam/Main.java
@@ -0,0 +1,12 @@
+package dk.sdu.dm847.exam;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        CsvParser parser = new CsvParser("example1.csv");
+        parser.parseStripComments();
+        parser.dumpTable("test_output.csv");
+        ToolUtils.runPEAX(new File("test_output.csv"), new File("output.csv"));
+    }
+}
